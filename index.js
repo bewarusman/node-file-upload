1bff5829186a859761c41429ea5de889f0a66ae1
Bewar Usman <bewar.usman@gmail.com>
2020-11-01 21:36:54 +0300

    added all routes

--- index.js
+++ index.js
@@ -3,25 +3,99 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const path = require("path");
+const fs = require("fs");
 
 // create and configure express app
 const app = express();
 app.use(bodyParser.json()); //parsing json files
 
-// configure mongodb
+// configuring mongoose
+mongoose.connect("mongodb://localhost:27017/blog_db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+// blog schema
+const blogSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  photo: { type: String, default: "None" },
+});
+// blog model
+const Blog = mongoose.model("blogs", blogSchema);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/blogs", (req, res) => res.json({ success: true, blogs: [] }));
-app.post("/blogs", (req, res) => res.json({ success: true, blog: {} }));
-app.put("/blogs/{id}", (req, res) => res.json({ success: true, blogs: [] }));
-app.delete("/blogs/{id}", (req, res) => res.json({ success: true }));
+// GET: /blogs
+app.get("/blogs", async (req, res) => {
+  try {
+    const { filter, skip, limit } = req.query;
+    const blogs = await Blog.find(filter).skip(skip).limit(limit);
+    res.json({ success: true, blogs });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      err,
+    });
+  }
+});
+
+// POST: /blogs
+app.post("/blogs", async (req, res) => {
+  try {
+    const blog = new Blog(req.body);
+    await blog.save();
+    res.json({ success: true, blog });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      err,
+    });
+  }
+});
+
+// PUT: /blogs/:id
+app.put("/blogs/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const blog = await Blog.findOneAndUpdate({ _id: id }, req.body);
+    if (blog == null) {
+      res.status(404).json({ success: false });
+      return;
+    }
+    res.json({ success: true, blog });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      err,
+    });
+  }
+});
+
+// DELETE /blogs/:id
+app.delete("/blogs/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const blog = await Blog.findOneAndDelete({ _id: id });
+    if (blog == null) {
+      res.status(404).json({ success: false });
+      return;
+    }
+    res.json({ success: true });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      err,
+    });
+  }
+});
 
 // uploading images
 const multer = require("multer");
 const uuid4 = require("uuid").v4;
 const storage = multer.diskStorage({
-  destination: path.join(__dirname, "/public/uploads"),
+  destination: path.join(__dirname, "public/uploads"),
   filename: function (req, file, cb) {
     const fullName =
       "blog_" + uuid4().replace(/-/g, "") + path.extname(file.originalname);
@@ -29,14 +103,31 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage: storage });
-app.post("/blogs/upload", upload.single("photo"), (req, res) =>
-  res.json({
-    success: true,
-    blog: {
-      photo: "/uploads/" + req.file.filename,
-    },
-  })
-);
+
+// POST /blogs/upload/:id
+app.post("/blogs/upload/:id", upload.single("photo"), async (req, res) => {
+  try {
+    const photo = "/uploads/" + req.file.filename;
+    const { id } = req.params;
+    const blog = await Blog.findOneAndUpdate({ _id: id }, { photo });
+    if (blog == null) {
+      fs.unlink(path.join(__dirname, photo), (err) => {
+        if (err) throw err;
+        else res.status(404).json({ success: false });
+      });
+      return;
+    }
+    res.json({
+      success: true,
+      blog,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      err,
+    });
+  }
+});
 
 const PORT = 3000;
 app.listen(PORT, (err) =>
